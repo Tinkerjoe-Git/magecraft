98edeaca8583347c14055d87e87e612fed39bc01
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-07-05 16:07:56 -0400

    Making Nav pretty

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,21 @@ import { connect } from 'react-redux'
 import { getCards } from '../actions/cards'
 import { fetchDecks } from '../actions/decks'
 import { logoutUser } from '../actions/auth'
-import { MenuList, MenuItem, Select } from '@material-ui/core'
+import {
+  MenuList,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  AppBar,
+  ToolBar,
+  IconButton,
+  InputBase,
+  Typography,
+} from '@material-ui/core'
+import { fade, makeStyles } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 
 class NavBar extends Component {
   state = {
@@ -56,77 +70,172 @@ class NavBar extends Component {
         value: 'decks',
       },
     ]
+    const useStyles = makeStyles((theme) => ({
+      formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+      },
+      selectEmpty: {
+        marginTop: theme.spacing(2),
+      },
+      root: {
+        flexGrow: 1,
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+      title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+          display: 'block',
+        },
+      },
+      search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+      },
+    }))
+    const classes = useStyles()
+
     const { search, dropdown } = this.state
     const { currentUser, loggedIn } = this.props
     return (
-      <div>
-        <MenuList>
-          <MenuItem
-            name="home"
-            component={Link}
-            to="/"
-            //onClick={this.handleItemClick}
+      <AppBar position="static">
+        <ToolBar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open menu"
           >
-            Home
-          </MenuItem>
-          {loggedIn && (
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            MageCraft
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <MenuList>
             <MenuItem
-              name="decks"
-              component={Link}
-              //to="/decks"
-              to={`/${currentUser.name}/decks`}
-              onClick={this.handleItemClick}
-            >
-              Decks
-            </MenuItem>
-          )}
-          <MenuItem>
-            <form onSubmit={this.handleSearch}>
-              <input
-                type="search"
-                name="search"
-                value={search}
-                onChange={this.handleChange}
-                placeholder={`Search ${dropdown}...`}
-              />
-            </form>
-            Search
-          </MenuItem>
-          <Select
-            position="relative"
-            name="dropdown"
-            onChange={this.handleChange}
-            options={options}
-            placeholder="Cards"
-          />
-          <MenuItem
-            component={Link}
-            onChange={this.handleChange}
-            options={options}
-            to="/cards"
-          >
-            Cards
-          </MenuItem>
-          {!loggedIn ? (
-            <MenuItem
-              component={Link}
-              to="/login"
-              //onClick={this.handleItemClick}
-            >
-              Login
-            </MenuItem>
-          ) : (
-            <MenuItem
+              name="home"
               component={Link}
               to="/"
-              onClick={this.handleLogout}
-              name="logout"
+              //onClick={this.handleItemClick}
             >
-              Logout
+              Home
             </MenuItem>
-          )}
-        </MenuList>
-      </div>
+            {loggedIn && (
+              <MenuItem
+                name="decks"
+                component={Link}
+                //to="/decks"
+                to={`/${currentUser.name}/decks`}
+                onClick={this.handleItemClick}
+              >
+                Decks
+              </MenuItem>
+            )}
+            <MenuItem>
+              <FormControl
+                classname={classes.formControl}
+                onSubmit={this.handleSearch}
+              >
+                <InputLabel
+                  type="search"
+                  name="search"
+                  value={search}
+                  onChange={this.handleChange}
+                  placeholder={`Search ${dropdown}...`}
+                />
+              </FormControl>
+              Search
+            </MenuItem>
+            <Select
+              className={classes.selectEmpty}
+              value="cards"
+              position="relative"
+              name="dropdown"
+              onChange={this.handleChange}
+              options={options}
+              placeholder="Cards"
+            />
+            <MenuItem
+              value="cards"
+              component={Link}
+              onChange={this.handleChange}
+              options={options}
+              to="/cards"
+            >
+              Cards
+            </MenuItem>
+            {!loggedIn ? (
+              <MenuItem
+                component={Link}
+                to="/login"
+                //onClick={this.handleItemClick}
+              >
+                Login
+              </MenuItem>
+            ) : (
+              <MenuItem
+                component={Link}
+                to="/"
+                onClick={this.handleLogout}
+                name="logout"
+              >
+                Logout
+              </MenuItem>
+            )}
+          </MenuList>
+        </ToolBar>
+      </AppBar>
     )
   }
 }
